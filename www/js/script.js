40e23709187380c605840f9a374bb76c1759f93e
Andrew Haith <andyhaith@gmail.com>
2017-02-10 14:26:46 +0000

    remove scope

--- www/js/script.js
+++ www/js/script.js
@@ -2,7 +2,7 @@ $(function(){
 	
 	if('serviceWorker' in navigator) {
 		navigator.serviceWorker
-				.register('./service-worker.js', {scope:'self-heal/www/'})
+				.register('./service-worker.js')
 				.then(function () {
 					console.log('Service worker registered');
 				});
