ad51162d31c6fe0cb437db61c7b8e3b69c0ae60f
Andrew Haith <andyhaith@gmail.com>
2017-02-10 13:49:48 +0000

    Specify scope of service worker

--- www/js/script.js
+++ www/js/script.js
@@ -2,7 +2,7 @@ $(function(){
 	
 	if('serviceWorker' in navigator) {
 		navigator.serviceWorker
-				.register('./service-worker.js')
+				.register('./service-worker.js', {scope:'/'})
 				.then(function () {
 					console.log('Service worker registered');
 				});
